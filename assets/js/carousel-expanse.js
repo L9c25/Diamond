9a9fe5d5137180ad63febd8333e6d430abe43f8b
Pedro D. <115499642+peuzada2@users.noreply.github.com>
2024-05-17 15:30:14 -0300

    att

--- assets/js/carousel-expanse.js
+++ assets/js/carousel-expanse.js
@@ -1,8 +1,26 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 4,
+    spaceBetween: 120,
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
+      type: "fraction",
     },
+
+    breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 4,
+      },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+  keyboard: true,
   });
